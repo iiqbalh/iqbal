0e3a25dc327b417d6d85e514e37bfe3eb1459406
iqbal <iqbalhernadi17@gmail.com>
2023-05-26 20:03:05 +0700

    menambah

---
+++ learning-javascript/week-one/day2-typeData/main.js
@@ -0,0 +1,18 @@
+let x = 10;
+let y = 20;
+let z = x + y;
+
+document.getElementById("demo").innerHTML = z;
+
+let myFirstName = "mochamad";
+let myMiddeName = "iqbal";
+let myLastName = "hernadi";
+
+console.log(myFirstName + ' ' + myMiddeName);
+
+let a = 5;
+let b = 5;
+let c = 6;
+
+document.getElementById("demo1").innerHTML =
+(a == b) + "<br>" + (a == c);
