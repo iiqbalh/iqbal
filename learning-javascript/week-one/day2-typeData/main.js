21aefa08c30807426f7e3d8b64121bc1e206c3cf
iqbal <iqbalhernadi17@gmail.com>
2023-05-27 12:48:23 +0700

    menambah

--- learning-javascript/week-one/day2-typeData/main.js
+++ learning-javascript/week-one/day2-typeData/main.js
@@ -21,3 +21,7 @@ function firstTime(){
     alert('hello word');
 }
 firstTime();
+
+let shoe = {name:"vans", size:"39", color:"black"};
+
+document.getElementById('demo2').innerHTML = "the name of shoe is " + shoe.name ;
---
+++ learning-javascript/week-one/day2-typeData/objek.js
@@ -0,0 +1,3 @@
+let shoe = {name:"vans", size:"39", color:"black"};
+
+document.getElementById('demo2').innerHTML = "the name of shoe is " + shoe.name ;
