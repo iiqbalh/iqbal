9da53039e1f8e80332fcd4a062d4f3b579236d6b
iqbal <iqbalhernadi17@gmail.com>
2023-05-28 09:37:06 +0700

    menambah

--- learning-javascript/week-one/day2-typeData/main.js
+++ learning-javascript/week-one/day2-typeData/main.js
@@ -24,4 +24,8 @@ firstTime();
 
 let shoe = {name:"vans", size:"39", color:"black"};
 
-document.getElementById('demo2').innerHTML = "the name of shoe is " + shoe.name ;
+document.getElementById('demo2').innerHTML = "the name of shoe is " + shoe.name;
+
+function displayDate(){
+    document.getElementById('demo3').innerHTML = Date();
+}
