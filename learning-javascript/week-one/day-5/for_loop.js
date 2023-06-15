c1f04c3026f5ee570e21d9983b7e631256f34bae
iqbal <iqbalhernadi17@gmail.com>
2023-06-15 21:10:21 +0700

    menmbah

---
+++ learning-javascript/week-one/day-5/for_loop.js
@@ -0,0 +1,20 @@
+let arr = [];
+let sum = 0;
+let sum2 = 0;
+
+for(let i = 0; i <= 100; i++){
+  if(i % 2 === 0){
+    sum += i
+  }
+}
+
+for(let i = 0; i <= 100; i++){
+  if(i % 2 === 1){
+    sum2 += i
+  }
+}
+
+arr.push(sum)
+arr.push(sum2)
+
+console.log(arr);
